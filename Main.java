fc8d67d81c3f4ec925c2b3c7e1a33ddc7d1f9fec
RafaelCastro23 <rafaelpuyo23@yahoo.com>
2026-06-25 10:54:42 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,36 @@
 // When you are ready to have your work checked, change the value below from N to Y
-// ReadyForSubmission=N
-// YOUR NAME:
+// ReadyForSubmission=Y
+// YOUR NAME: Rafael Castro
 // COLLABORATORS:
-// DATE:  
+// DATE:  6/25/26
 
 public class Main
 {
 	public static void main(String[] args)
 	{
+		String questionNum1 = "Should a self-driving car prioritize the safety of its passengers or pedestrians?";
+		String questionNum2 = "What factors should the car's algorithms consider when making such determinations?";
+		String questionNum3 = "Who should be deciding the algorithms for self-driving cars?";
+		String answerNum1 = "The safety of the pedestrians because when the passengers gets into the vehicle.";
+		String answerNum2 = "Driver's speed and location, this will be best for passengers travel.";
+		String answerNum3 = "A group of experienced programmers with diverse background in multi-culture knowledge.";
+
 		System.out.println("My thoughts on the article are:");
+		System.out.printf("%s%n%s%n" ,questionNum1, answerNum1);
+		System.out.printf("%s%n%s%n" ,questionNum2, answerNum2);
+		System.out.printf("%s%n%s%n" ,questionNum3, answerNum3);
+
 		// Part 2: Invoking/calling method
-		
+
 	}
-	
+
 	// Part 1: Creating method definition
-	
+	//method header Main
+	//method body Calling Static with Local Variables
+	//return statement
+	// System.out.println("My thoughts on the article are:");
+	//	System.out.printf("%s%n%s%n" ,questionNum1, answerNum1);
+	//	System.out.printf("%s%n%s%n" ,questionNum2, answerNum2);
+	//	System.out.printf("%s%n%s%n" ,questionNum3, answerNum3);
+	//method identifier
 }
